02ec06af83544993f486542a1283e4f3b406ab80
Jonathan Jalouzot <jalouzot@lemonde.fr>
2016-07-04 14:35:06 +0200

    feat: add service worker to offline

--- server.js
+++ server.js
@@ -39,6 +39,18 @@ function renderPage(appHtml) {
     <link rel=stylesheet href=/index.css>
     <div id=app>${appHtml}</div>
     <script src="/bundle.js"></script>
+    <script>
+    if('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/sw.js', { scope: '/' })
+          .then(function(registration) {
+                console.log('Service Worker Registered');
+          });
+
+        navigator.serviceWorker.ready.then(function(registration) {
+           console.log('Service Worker Ready');
+        });
+      }
+    </script>
    `
 }
 
