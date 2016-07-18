12253763478636adf3cea253c2db7a8ca41ab728
Jonathan Jalouzot <jonathan.jalouzot@gmail.com>
2016-07-18 18:13:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Router, browserHistory } from 'react-router'
 // import routes and pass them into <Router/>
-import routes from './modules/routes'
+import routes from './modules/Routes'
 
 render(
   <Router routes={routes} history={browserHistory}/>,
